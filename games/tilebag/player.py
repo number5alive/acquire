82fd3ce08a5f0c3a6c6192037753d373b12ec1b3
Steve <number5alive@gmail.com>
2020-05-07 15:42:48 -0400

    split TileBagPlayer and TileBagGame into separate files

--- games/tilebag/player.py
+++ games/tilebag/player.py
@@ -1,45 +1,90 @@
-from games.tilebag.tiles import Tile
+from games.tilebag.tiles import Tile, TileBag
+from base import Player
 
-class Player:
-  def __init__(self, id, name=None):
-    self.id=id
-    self.cash=6000
-    self.tiles=[]
-    self.stocks=[]
-    if name is not None:
-      self.name=name
-    else:
-      self.name="NoName"+str(id)
+class TileBagPlayer(Player):
+  _tiles=[]
+  _stocks=[]
 
-  def getId(self):
-    return self.id
-
-  def getName(self):
-    return self.name
-
-  def setName(self, name):
-    self.name=name
-
-  def playTile(self, t):
-    pass
-
-  def receiveTile(self, t):
-    self.tiles.append(t)
-
-  def __repr__(self):
-    return 'Player ' + str(self.id) + ':$' + str(self.cash) + ',Tiles:' + str(self.tiles)
+  def __init__(self, id, name=None, money=6000):
+    super().__init__(id, name=name)
+    self._startmoney = money
+    self._initcomponents()
      
-  def serialize(self):
+  def __del__(self):
+    print("Deleting TileBagPlayer")
+
+  def _initcomponents(self):
+    self._tiles = []
+    self._stocks = []
+    self.money = self._startmoney
+
+  def reset(self):
+    del self._tiles
+    del self._stocks
+    self._initcomponents()
+
+  # this only exists to help program the tests
+  # We'll need a way for the user to specify which tile they're playing
+  # NOTE: We don't pop it, the game will do that if it's a valid move
+  def selectRandomTile(self):
+    if len(self._tiles) > 0:
+      shuffle(self._tiles)
+      return self._tiles[0]
+    else:
+      return None
+
+  def receiveTile(self, tile):
+    self._tiles.append(tile)
+
+  def removeTile(self, tile):
+    self._tiles.remove(tile)
+
+  def numStocks(self, hname=None):
+    if hname is None:
+      return len(self._stocks)
+    else:
+      count=0
+      for s in self._stocks:
+        if s.name == hname:
+          count+=1
+      return count
+
+  def receiveStock(self, stock):
+    self._stocks.append(stock)
+
+  def returnStock(self, sname):
+    for s in self._stocks:
+      if s.name == sname:
+        self._stocks.remove(s)
+        return s
+    return None
+
+  @property
+  def tiles(self):
+    return self._tiles
+
+  def loadFromSavedData(self,sd):
+    self._name=sd['name']
+     
+    pd=sd['playerdata']
+    for tile in pd['tiles']:
+      t=Tile.newTileFromAlpha(tile)
+      self._tiles.append(t)
+     
+  def savePlayerData(self):
+    # recover unique names in our list of stocks
+    snames={s.name for s in self._stocks}
     return {
-        'id': self.id,
-        'name': self.name,
-        'num_tiles': len(self.tiles),
-     }
- 
+      'tiles': [t.serialize() for t in self._tiles],
+      'money': self.money,
+      'stocks': { s : sum(x.name == s for x in self._stocks) for s in snames},
+      }
+       
 if __name__ == "__main__":
-  p=Player(17)
+  p=TileBagPlayer(100, "Stan", money=7000)
 
-  t = Tile(7,7)
-  p.receiveTile(t)
-
-  print(p)
+  # TODO: add a bunch of unit tests (most are covered now in tilebag.py
+   
+  print("---- Testing TileBagPlayer functionality ----")
+  print("savePlayerData: {}".format(p.savePlayerData()))
+   
--- games/tilebag/tilebag.py
+++ games/tilebag/tilebag.py
@@ -1,5 +1,6 @@
 from base import Game
 from base import Player
+from games.tilebag.player import TileBagPlayer
 from games.tilebag.tiles import Tile, TileBag
 from games.tilebag.board import Board
 from games.tilebag.hotels import Hotel, Stock, HOTELS
@@ -7,84 +8,6 @@ from games.tilebag.gamelog import GameLog, GameAction
 from itertools import cycle, islice
 from random import shuffle
 
-class TileBagPlayer(Player):
-  _tiles=[]
-  _stocks=[]
-
-  def __init__(self, id, name=None, money=6000):
-    super().__init__(id, name=name)
-    self._startmoney = money
-    self._initcomponents()
-     
-  def __del__(self):
-    print("Deleting TileBagPlayer")
-
-  def _initcomponents(self):
-    self._tiles = []
-    self._stocks = []
-    self.money = self._startmoney
-
-  def reset(self):
-    del self._tiles
-    del self._stocks
-    self._initcomponents()
-
-  # this only exists to help program the tests
-  # We'll need a way for the user to specify which tile they're playing
-  # NOTE: We don't pop it, the game will do that if it's a valid move
-  def selectRandomTile(self):
-    if len(self._tiles) > 0:
-      shuffle(self._tiles)
-      return self._tiles[0]
-    else:
-      return None
-
-  def receiveTile(self, tile):
-    self._tiles.append(tile)
-
-  def removeTile(self, tile):
-    self._tiles.remove(tile)
-
-  def numStocks(self, hname=None):
-    if hname is None:
-      return len(self._stocks)
-    else:
-      count=0
-      for s in self._stocks:
-        if s.name == hname:
-          count+=1
-      return count
-
-  def receiveStock(self, stock):
-    self._stocks.append(stock)
-
-  def returnStock(self, sname):
-    for s in self._stocks:
-      if s.name == sname:
-        self._stocks.remove(s)
-        return s
-    return None
-
-  @property
-  def tiles(self):
-    return self._tiles
-
-  def loadFromSavedData(self,sd):
-    self._name=sd['name']
-     
-    pd=sd['playerdata']
-    for tile in pd['tiles']:
-      t=Tile.newTileFromAlpha(tile)
-      self._tiles.append(t)
-     
-  def savePlayerData(self):
-    # recover unique names in our list of stocks
-    snames={s.name for s in self._stocks}
-    return {
-      'tiles': [t.serialize() for t in self._tiles],
-      'money': self.money,
-      'stocks': { s : sum(x.name == s for x in self._stocks) for s in snames},
-      }
 
 class TileBagGame(Game):
   # Fixed Data about the Game
@@ -457,11 +380,6 @@ if __name__ == "__main__":
   print("too much - Money: {}".format(not tbg.moneyAction(100, -100000 )))
 
   print("savePlayerData: {}".format(p.savePlayerData()))
-   
-  print("---- Testing TileBagPlayer functionality ----")
-  print("savePlayerData: {}".format(p.savePlayerData()))
-  print(">>> serialize: {}".format(tbg.serialize(True)))
-  print(">>> getPublicInformation: {}".format(tbg.getPublicInformation()))
    
   print("---- Testing TileBagGame reset functionality ----")
   print(">>> saveGameData: {}".format(tbg.saveGameData()))
