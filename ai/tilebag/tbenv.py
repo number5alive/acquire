0fcc2010ca9084938e64a046ab04c9f2abcfe9ac
Steve <number5alive@gmail.com>
2021-01-23 06:18:37 +0000

    openai: sync's threads - will only return from 'reset' or 'step' once it's our turn again (or on fail)

--- ai/tilebag/tbenv.py
+++ ai/tilebag/tbenv.py
@@ -2,11 +2,40 @@ import gym
 from gym import spaces
 import random
 from games.tilebag.hotels import HOTELS
+from games.tilebag.tilebag import TileBagGame
 from ai.tilebag.lobbyREST import LobbyREST
 from ai.tilebag.aiplayer_base import TileBagBASEAIPlayer
+from threading import Lock, Event
 
 class TileBagEnv(gym.Env, TileBagBASEAIPlayer):
-  """ An OpenAI Gym enviornment that knows about TileBag """
+  """ An OpenAI Gym enviornment that knows about TileBag 
+
+      This class implements:
+        - gym.Env, so we can tie into the openAI gym; and
+        - TileBagBASEAIPlayer, so we don't have to handle all the gamestate stuff
+
+      Basically... we have:
+        - a thread running that's waiting for an "it's your turn" message; and
+        - an openAI gym class that should take steps and return the new state
+
+      What we'll do is:
+        - lock on access to gamedata (so only one thread plays with it at a time)
+        - flag when new data comes in - so we can return from the previous step call
+
+      turnHandler - websocket thread pseudo-logic:
+        - on turnHandler (we got called with new state information because it's our turn)
+          - flag that new state info is available
+          - wait for the lock so we can update the state info
+
+      openai step - machine learning step, presumes it's our turn:
+        - take action
+        - if (200-OK)
+          - unlock state
+          - wait for flag
+          - lock on state
+          - return state # we still have the lock
+        - else (no worries, state won't change, we didn't do anything!)
+  """
      
   ROWNAME=[chr( ord('A') + x ) for x in range(0,8)]
   HOTELNAME=[h['name'] for h in HOTELS]
@@ -20,28 +49,69 @@ class TileBagEnv(gym.Env, TileBagBASEAIPlayer):
 
     # TODO grab these directly from games.tilebag.tilebag.ACTIONS
     actions={
-        'placetile': spaces.MultiDiscrete([12, 8]), #col, row
-        'placehotel': spaces.Discrete(7),
-        'removehotel': spaces.Discrete(7),
-        'buystock': spaces.MultiDiscrete([7, 3]),  #max buy is three
-        'sellstock': spaces.MultiDiscrete([7, 25]), #can sell up to all holding
+        TileBagGame.EVENT_PLACETILE: spaces.MultiDiscrete([12, 8]), #col, row
+        TileBagGame.EVENT_PLACEHOTEL: spaces.Discrete(7),
+        TileBagGame.EVENT_REMOVEHOTEL: spaces.Discrete(7),
+        TileBagGame.EVENT_BUYSTOCKS: spaces.MultiDiscrete([7, 3]),  #max buy is three
+        TileBagGame.EVENT_SELLSTOCKS: spaces.MultiDiscrete([7, 25]), #can sell up to all holding
         'declare': spaces.Discrete(3), # 0=NOP, 1=Declare Game End, 2=End Turn
         }
     self.action_space = spaces.Dict(actions)
 
     self.observation_space = spaces.Dict({
         'ourturn': spaces.Discrete(1),
-        'tbstate': spaces.Discrete(8),
+        'tbstate': spaces.Discrete(len(TileBagGame.STATES)),
         })
+    self.lockStateChange=Lock()
+    self.evtOurTurn=Event()
+
+  def _waitForOurTurn(self):
+    """ synchronizes between the websocket and the main thread
+        doesn't return until it's our turn"""
+    print("Just waiting for our turn to come around")
+    self.evtOurTurn.wait()
+    self.lockStateChange.acquire() # will block until we get it! keep it until next go-around
+    self.evtOurTurn.clear()
+
+    self.currobs=self._toObservationState()
+    print("new state: {}".format(self.currobs))
 
   def reset(self):
     """ Reset a game to the start so we can run again """
+    print("resetting the game")
     self.lobby.restartGame(self.gameid)
+    self._waitForOurTurn()
+
+  def _takeAction(self, action):
+    """ issues the rest call into the game engine and returns True if 200-OK response """
+    return False
+
+  def _toObservationState(self):
+    """ converts the game json into the openai gym.Spaces.Dict type for this env """
+    return { 'ourturn' : 1 if self.currplayer == self.playerid else 0,
+             'tbstate' : TileBagGame.STATES.index(self.currstate) }
+
+  def _gameIsOver(self):
+    """ returns true if the game is over """
+    return self.currstate == TileBagGame.STATE_ENDGAME
 
   def step(self, action):
     """ Needs to return: observation, reward, done, info """
+    reward=-1 #presume invalid move
     s_act, s_args = self._getActionStrings(action)
     print("Action: {}, {}".format(s_act, s_args))
+
+    if self._takeAction(action):
+      # we made a valid move, wait until it's our turn again before returning to the caller
+      self.lockStateChange.release() # okay, safe to let the game update this again
+       
+      # wait until our turn comes around again
+      self._waitForOurTurn()
+      reward=1
+
+    # if it was a good move, the state will be different and reward will be positive
+    # if is was an invalid move, the state will be the same and the reward will be negative
+    return self.currobs, reward, self._gameIsOver(), None
 
   def render(self, mode='human', close=False):
     pass
@@ -50,23 +120,30 @@ class TileBagEnv(gym.Env, TileBagBASEAIPlayer):
     """ from TileBagBASEAIPlayer - called when it's our turn to act 
         NOTE: called from the websocket thread """
     # TODO: flag to the main thread that we have data to consume
-    print("Hmm... our turn to do something!")
+    if self.currplayer == self.playerid:
+      print("Hmm... our turn to do something!")
+
+      with self.lockStateChange:
+        self.evtOurTurn.set()
 
   def _tileName(self, coords):
     """ TileBag has 12 numbered columns, and 8 lettered rows, this mixes me up all the time! """
-    return str(coords[0]) + TileBagEnv.ROWNAME[coords[1]]
+    return str(coords[0]+1) + TileBagEnv.ROWNAME[coords[1]]
 
   def _getActionStrings(self, action):
     """ Helper function to return a nice printable version of the action being taken 
         returns: action, args """
     args=""
-    if action[0]=='placetile': args=self._tileName(action[1])
-    elif action[0]=='placehotel': args=TileBagEnv.HOTELNAME[action[1]]
-    elif action[0]=='declare': args=TileBagEnv.DECLAREACTION[action[1]]
-    elif action[0]=='buystock':
-        args=TileBagEnv.HOTELNAME[action[1][0]] + ", " + str(action[1][1])
-    elif action[0]=='sellstock':
-        args=TileBagEnv.HOTELNAME[action[1][0]] + ", -" + str(action[1][1])
+    if action[0]==TileBagGame.EVENT_PLACETILE:
+        args=self._tileName(action[1])
+    elif action[0]==TileBagGame.EVENT_PLACEHOTEL or action[0]==TileBagGame.EVENT_REMOVEHOTEL:
+        args=TileBagEnv.HOTELNAME[action[1]]
+    elif action[0]==TileBagGame.EVENT_BUYSTOCKS:
+        args=TileBagEnv.HOTELNAME[action[1][0]] + ", " + str(action[1][1]+1)
+    elif action[0]==TileBagGame.EVENT_SELLSTOCKS:
+        args=TileBagEnv.HOTELNAME[action[1][0]] + ", -" + str(action[1][1]+1)
+    elif action[0]=='declare':
+        args=TileBagEnv.DECLAREACTION[action[1]]
          
     return action[0], args
 
@@ -74,9 +151,16 @@ class TileBagEnv(gym.Env, TileBagBASEAIPlayer):
 # for running tests
 if __name__ == "__main__":
   import random
-
   
   tbe=TileBagEnv(None,None,None)
+
+  print("===== Testing the action_space =====")
+  for i in range(0,100):
+    action = random.choice(list(tbe.action_space.sample().items()))
+    s_act, s_args = tbe._getActionStrings(action)
+    print("Action: {}, {}".format(s_act, s_args))
+     
+  print("===== Kicking off an openai player =====")
   aithread=tbe.runAILoop()
   tbe.reset()
   for i in range(0,10):
@@ -88,3 +172,4 @@ if __name__ == "__main__":
     print("Trying to Exit Cleanly...")
     tbe.killAILoop()
     print("Exitting")
+     
